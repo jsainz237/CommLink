9537a5f8d276fdb005c465c2cd0e716e176d4951
Jesse <jsainz237@gmail.com>
2018-10-31 20:55:07 -0500

    added landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'landing_page#index'
   resources :instructors
   resources :courses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
