57c7496ad137beb05858011b3f966481611a15d5
jjs0310 <jjs0310@dynamic-10-125-187-96.dynamic.unt.edu>
2018-11-08 12:04:52 -0600

    trying to add ratings to courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -69,6 +69,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:name, :number, :description, {instructor_ids: []})
+      params.require(:course).permit(:name, :rating, :number, :description, {instructor_ids: []})
     end
 end
